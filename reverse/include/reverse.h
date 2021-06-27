7295677735b7f083ff061af5ac8493e396d97fd3
Oscar Kemuel <oscar.kemuel5@gmail.com>
2021-06-27 19:50:56 -0300

    reverse completed

--- reverse/include/reverse.h
+++ reverse/include/reverse.h
@@ -10,14 +10,35 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @tparam Itr iterator para o range.
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ *
  */
-template<class BidirIt>
-void reverse(BidirIt first, BidirIt last)
-{
-    // TODO
-}
+template < typename Itr >
+void reverse( Itr first, Itr last ){
+  // iter_swap: Troca dois valores referenciados por um par de iteradores especificados.
+  // [ 1, 2, 3, 4, 5, 6 ]
+  // --last
+  // [ > 1, 2, 3, 4, 5, 6 < ]
+  // ++first
+  // --last
+  // [ 6, > 2, 3, 4, 5 <, 1 ]
+  // ++first
+  // --last
+  // [ 6, 5, > 3, 4 < , 2, 1 ]
+  // [ 6, 5, 4, 3, 2, 1 ]
+
+  while (first != last && first != --last) {
+    // std::cout << *first << " -> " << *last << endl;
+    std::iter_swap (first,last);
+    ++first;
+  }
+    // std::cout << endl;
+};
+
 
 }
 #endif
