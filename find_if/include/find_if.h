b0846664b4a174eaee8ad3d344fefbb5b731054b
Oscar Kemuel <oscar.kemuel5@gmail.com>
2021-07-11 16:17:52 -0300

    feat: find_if completed

--- find_if/include/find_if.h
+++ find_if/include/find_if.h
@@ -10,14 +10,25 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @tparam InputIt iterator para o range.
+ * @tparam UnaryPredicate função que retorna objeto booleano;
+ *
+ * @param first Ponteiro para o primeiro elemento do range a ser considerado
+ * @param last Ponteiro para a posição logo após o último elemento do range ser considerado
+ * @param p Função para testar se um valor no range satisfaz o predicado p
  */
 template<class InputIt, class UnaryPredicate>
-InputIt find_if(InputIt first, InputIt last, UnaryPredicate p)
-{
-    // TODO
-    return first;
+InputIt find_if(InputIt first, InputIt last, UnaryPredicate p){
+  for (InputIt i = first; i != last; ++i){
+    if (p(*first)){
+      return first;
+    }
+
+    ++first;
+  }
+
+  return last;
 }
 
 }
