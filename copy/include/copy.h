2376217efebb4b38b0d6e4119181967bd9c698e5
Oscar Kemuel <oscar.kemuel5@gmail.com>
2021-07-11 13:40:29 -0300

    feat: completed copy

--- copy/include/copy.h
+++ copy/include/copy.h
@@ -10,14 +10,31 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @tparam InputIt iterator para o range.
+ *
+ * @param first Ponteiro para o primeiro elemento do range a ser considerado
+ * @param last Ponteiro para a posição logo após o último elemento do range ser considerado
+ * @param d_first O inicio do range onde os elementos devem ser copiados.
  */
 template<class InputIt>
-InputIt copy(InputIt first, InputIt last, InputIt d_first)
-{
+InputIt copy(InputIt first, InputIt last, InputIt d_first){
     // TODO
-    return first;
+    InputIt d_last = d_first;
+
+    while(true){
+      // std::cout << *first << " -> " << *last << " " << *d_first << endl;
+      if(first != last){
+        *d_last = *first;
+
+        ++d_last;
+        ++first;
+      }else {
+        break;
+      }
+    }
+
+    return d_last;
 }
 
 }
